5d31321c6f3f26a178794bafb03495f4a90b6b51
samuel cesar <samuelcesar156@gmail.com>
2024-10-14 03:31:24 -0300

    atualização de codigo

--- recipe-api/src/app.js
+++ recipe-api/src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors'; // Importa o pacote CORS
 import router from './routes/index.js';
 
 const app = express();
 const port = 3000;
+
+// Usar CORS para permitir requisições de diferentes origens
+app.use(cors()); // Permite CORS para todas as rotas
 
 app.use('/api', router);
 
